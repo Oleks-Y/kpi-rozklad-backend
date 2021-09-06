d3ecd9a7a83ba58057f6073e2c61235a397489da
Oleksandr Yatsevskiy <alex@mamba.local>
2021-09-06 21:36:48 +0300

    group record and some shit

--- src/Rozklad.Parser/Entities/ApiXmlResponse.cs
+++ src/Rozklad.Parser/Entities/ApiXmlResponse.cs
@@ -7,15 +7,15 @@ namespace Rozklad.Parser.Entities
     {
         [XmlElement("string")]
         public string String { get; set; }
-        
+        [XmlIgnore]
         public T Data { get; set; }
         /// <summary>
         /// // TODO if parameters can`t automatically pass to consstructor rename to method like 'init' and call from code
         /// /// </summary>
         /// <param name="data"></param>
-        public ApiXmlResponse(string data)
+        public ApiXmlResponse()
         {
-            Data = JsonSerializer.Deserialize<T>(data);
+            Data = JsonSerializer.Deserialize<T>(String);
         }
     }
 }
